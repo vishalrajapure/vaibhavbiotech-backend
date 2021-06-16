4c80b88045f73b3b6a941c81fcff7c1975e2ed91
vishalrajapure <vishalrajapure@gmail.com>
2021-06-16 23:42:03 +0530

    First commit

---
+++ src/main/java/com/vaibhavbiotech/VaibhavbiotechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhavbiotech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaibhavbiotechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaibhavbiotechBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaibhavbiotech/VaibhavbiotechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhavbiotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaibhavbiotechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
