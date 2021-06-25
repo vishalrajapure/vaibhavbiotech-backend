7cf3f2b09598c946cbd201778a1e0b6e7e102b6b
vishalrajapure <vishalrajapure@gmail.com>
2021-06-25 16:25:32 +0530

    adding cross origin

--- src/main/java/com/vaibhavbiotech/controllers/ContactUsController.java
+++ src/main/java/com/vaibhavbiotech/controllers/ContactUsController.java
@@ -3,15 +3,13 @@ package com.vaibhavbiotech.controllers;
 import com.vaibhavbiotech.models.ContactUs;
 import com.vaibhavbiotech.services.ContactUsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/ContactUs")
 public class ContactUsController {
 
